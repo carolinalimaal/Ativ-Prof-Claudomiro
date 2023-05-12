813043a0f4a0f22ad4bb2b82bf415b94b0ba33c4
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2023-05-12 17:45:56 -0300

    Add files via upload

---
+++ Atividades Extra 2ªAVA/Simulador de memória/Simulador de memória.py
@@ -0,0 +1,44 @@
+memory = ['00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000']
+
+def write(address, data):
+    memory[address] = data
+    print()
+
+def read(address):
+    print(memory[address])
+    print()
+
+def showAll():
+    print('TODOS OS ENDEREÇOS: ')
+    for i in range(0, len(memory)):
+        print(memory[i])
+    print()
+
+address = 0
+add = 0
+data = 0
+
+while True:
+    operacao = input("""[W] GRAVAR
+[R] LER
+[L] LISTAR TODA A MEMÓRIA
+[S] SAIR
+ESCOLHA UMA OPÇÃO:  """)
+    print()
+    if operacao in 'Ww':
+        address = input('INSIRA O ENDEREÇO DE 4 BITS: ')
+        address = int(address, 2)
+        data = input('INSIRA O DADO DE 8 BITS: ')
+        memory[address] = data 
+        write(address, data)
+
+    elif operacao in 'Rr':
+        address = input('INSIRA O ENDEREÇO DE 4 BITS: ')
+        address = int(address, 2)
+        read(address)
+
+    elif operacao in 'Ll':
+        showAll()
+    else:
+        print('FECHANDO...')
+        break
